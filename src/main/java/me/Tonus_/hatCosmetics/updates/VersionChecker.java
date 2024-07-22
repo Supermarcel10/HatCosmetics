5c1a31420ff72dee1dafb6be579dd4c00d1f6c78
Supermarcel10 <marcelbarlik@gmail.com>
2024-07-22 20:09:12 +0100

    - Moved VersionChecker to its own package.

--- src/main/java/me/Tonus_/hatCosmetics/updates/VersionChecker.java
+++ src/main/java/me/Tonus_/hatCosmetics/updates/VersionChecker.java
@@ -1,13 +1,8 @@
-package me.Tonus_.hatCosmetics.utility;
+package me.Tonus_.hatCosmetics.updates;
 
 import me.Tonus_.hatCosmetics.HatCosmetics;
 import me.Tonus_.hatCosmetics.networking.ModrinthAPIClient;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.jar.Manifest;
 
 
 public class VersionChecker {
