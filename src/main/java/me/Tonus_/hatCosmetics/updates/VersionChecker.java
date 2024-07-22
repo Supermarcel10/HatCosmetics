0c3f139bd13f90842894ae9aa6caf0c83b7471ed
Supermarcel10 <marcelbarlik@gmail.com>
2024-07-22 21:06:32 +0100

    - Added documentation to VersionChecker. - Refactored VersionChecker::compareVersions to be more definitive. - Reversed the output of VersionChecker::compareVersions.

--- src/main/java/me/Tonus_/hatCosmetics/updates/VersionChecker.java
+++ src/main/java/me/Tonus_/hatCosmetics/updates/VersionChecker.java
@@ -6,6 +6,10 @@ import org.jetbrains.annotations.NotNull;
 
 
 public class VersionChecker {
+	/**
+	 * Checks for updates for the plugin
+	 * @param projectID the Modrinth project ID of the plugin
+	 */
 	public static void checkForUpdates(String projectID) {
 		String remoteVersion = ModrinthAPIClient.getLatestPluginVersion(projectID);
 		if (remoteVersion == null) {
@@ -16,32 +20,42 @@ public class VersionChecker {
 		String pluginVersion = getPluginVersion();
 
 		int versionComparison = compareVersions(pluginVersion, remoteVersion);
-		if (versionComparison < 0) {
-			HatCosmetics.getLog().warn("A new version is available: " + remoteVersion);
-			HatCosmetics.getLog().warn("You are currently running: " + pluginVersion);
+		if (versionComparison > 0) {
+			HatCosmetics.getLog().warn("A new version is available: {}", remoteVersion);
+			HatCosmetics.getLog().warn("You are currently running: {}", pluginVersion);
 			HatCosmetics.getLog().warn("Update here: https://modrinth.com/plugin/hatcosmetics/version/latest");
-		} else if (versionComparison > 0) {
+		} else if (versionComparison < 0) {
 			HatCosmetics.getLog().warn("You are running an unknown version!");
 			HatCosmetics.getLog().warn("Ensure you download the plugin from trusted sources.");
 		}
 	}
 
+	/**
+	 * Retrieves the version of the plugin
+	 * @return the version of the plugin
+	 */
 	private static @NotNull String getPluginVersion() {
 		return HatCosmetics.getInstance().getDescription().getVersion();
 	}
 
-	private static int compareVersions(@NotNull String v1, @NotNull String v2) {
-		String[] v1Parts = v1.split("\\.");
-		String[] v2Parts = v2.split("\\.");
+	/**
+	 * Compares two versions
+	 * @param local the local version
+	 * @param remote the remote version
+	 * @return -1 if unknown version, 0 if up to date, 1 if it needs updating
+	 */
+	protected static int compareVersions(@NotNull String local, @NotNull String remote) {
+		String[] localParts = local.split("\\.");
+		String[] remoteParts = remote.split("\\.");
 
-		int length = Math.max(v1Parts.length, v2Parts.length);
+		int length = Math.max(localParts.length, remoteParts.length);
 
 		for (int i = 0; i < length; i++) {
-			int v1Part = i < v1Parts.length ? Integer.parseInt(v1Parts[i]) : 0;
-			int v2Part = i < v2Parts.length ? Integer.parseInt(v2Parts[i]) : 0;
+			int localPart = i < localParts.length ? Integer.parseInt(localParts[i]) : 0;
+			int remotePart = i < remoteParts.length ? Integer.parseInt(remoteParts[i]) : 0;
 
-			if (v1Part < v2Part) return -1;
-			else if (v1Part > v2Part) return 1;
+			if (localPart < remotePart) return 1;
+			else if (localPart > remotePart) return -1;
 		}
 
 		return 0;
