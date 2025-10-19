c7ddc626f0fb512699411057323eeab22e949e5d
marcel <47798787+Supermarcel10@users.noreply.github.com>
2025-10-19 13:10:13 +0100

    - Made MessageHandler a singleton instance

--- src/main/java/me/Tonus_/hatCosmetics/utility/MessageHandler.java
+++ src/main/java/me/Tonus_/hatCosmetics/utility/MessageHandler.java
@@ -1,15 +1,14 @@
 package me.Tonus_.hatCosmetics.utility;
 
-import me.Tonus_.hatCosmetics.HatCosmetics;
+import me.Tonus_.hatCosmetics.config.ConfigHandler;
 import net.kyori.adventure.text.Component;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
-
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
@@ -17,29 +16,36 @@ import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
 
-public class Messages {
+public class MessageHandler {
+	private final Plugin plugin;
+	private final ConfigHandler configHandler;
 	private static String serverLocale;
 	private static final Map<String, String> generics = new HashMap<>();
 	private static final Map<String, FileConfiguration> translations = new HashMap<>();
 
-	public static void init() {
-		String localeConfig = Configs.getString("locale");
+	public MessageHandler(Plugin plugin, ConfigHandler configHandler) {
+		this.plugin = plugin;
+		this.configHandler = configHandler;
 
+		init();
+	}
+
+	private void init() {
 		// Default to en_US if the locale is not found
-		serverLocale = localeConfig == null ? "en_US" : localeConfig;
+		serverLocale = configHandler.getStringOrDefault("locale", "en_US");
 
 		ensureTemplateExists();
 		loadGenerics();
 		loadAllTranslations(); // TODO: Instead of loading all translations, load only the server locale, and then load the rest when needed (e.g. when a player joins)
 		loadLocalTranslations();
 
-		HatCosmetics.getLog().info("Loaded " + translations.size() + " translations.");
+        plugin.getSLF4JLogger().info("Loaded {} translations.", translations.size());
 	}
 
 	/**
 	 * Reloads all translations
 	 */
-	public static void reload() {
+	public void reload() {
 		translations.clear();
 		init();
 	}
@@ -47,17 +53,17 @@ public class Messages {
 	/**
 	 * Ensures that the template file exists
 	 */
-	private static void ensureTemplateExists() {
+	private void ensureTemplateExists() {
 		// Check if the messages directory exists
-		File messageDir = new File(HatCosmetics.getInstance().getDataFolder(), "messages");
+		File messageDir = new File(plugin.getDataFolder(), "messages");
 		if (!messageDir.exists()) {
-			HatCosmetics.getInstance().saveResource("messages/", false);
+			plugin.saveResource("messages/", false);
 		}
 
 		// Check if the template file exists
 		File templateFile = new File(messageDir, "template.yml");
 		if (!templateFile.exists()) {
-			HatCosmetics.getInstance().saveResource("messages/template.yml", false);
+			plugin.saveResource("messages/template.yml", false);
 		}
 	}
 
@@ -65,14 +71,14 @@ public class Messages {
 	 * Loads all local translations
 	 */
 	@SuppressWarnings("ResultOfMethodCallIgnored")
-	private static void loadLocalTranslations() {
-		File messageDir = new File(HatCosmetics.getInstance().getDataFolder(), "messages");
+	private void loadLocalTranslations() {
+		File messageDir = new File(plugin.getDataFolder(), "messages");
 		if (!messageDir.exists()) {
 			messageDir.mkdirs();
 		}
 
 		if (!messageDir.isDirectory()) {
-			HatCosmetics.getLog().error("Messages is not a directory!");
+			plugin.getSLF4JLogger().error("Messages is not a directory!");
 			return;
 		}
 
@@ -92,8 +98,8 @@ public class Messages {
 	/**
 	 * Loads all generic messages
 	 */
-	private static void loadGenerics() {
-		try (InputStream inputStream = Messages.class.getClassLoader().getResourceAsStream("messages/generics.yml")) {
+	private void loadGenerics() {
+		try (InputStream inputStream = MessageHandler.class.getClassLoader().getResourceAsStream("messages/generics.yml")) {
 			if (inputStream != null) {
 				YamlConfiguration genericYAML = YamlConfiguration.loadConfiguration(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
 
@@ -102,18 +108,17 @@ public class Messages {
 				}
 			} else throw new IOException("Failed to open input stream!");
 		} catch (IOException e) {
-			HatCosmetics.getLog().error("Failed to load generic messages!");
-			HatCosmetics.getLog().error(e.toString());
+			plugin.getSLF4JLogger().error("Failed to load generic messages! {}", e.toString());
 		}
 
-		HatCosmetics.getLog().info("Loaded " + generics.size() + " generic messages.");
+        plugin.getSLF4JLogger().info("Loaded {} generic messages.", generics.size());
 	}
 
 	/**
 	 * Loads all generic translations from the JAR file
 	 */
-	private static void loadAllTranslations() {
-		try (JarFile jarFile = new JarFile(HatCosmetics.class.getProtectionDomain().getCodeSource().getLocation().getPath())) {
+	private void loadAllTranslations() {
+		try (JarFile jarFile = new JarFile(plugin.getClass().getProtectionDomain().getCodeSource().getLocation().getPath())) {
 			Enumeration<JarEntry> entries = jarFile.entries();
 
 			while (entries.hasMoreElements()) {
@@ -121,40 +126,25 @@ public class Messages {
 				if (isValidTranslationFile(entry)) loadTranslationFile(entry);
 			}
 		} catch (IOException e) {
-			HatCosmetics.getLog().error("Failed to load translations from JAR file!");
-			HatCosmetics.getLog().error(e.toString());
+			plugin.getSLF4JLogger().error("Failed to load translations from JAR file! {}", e.toString());
 		}
-	}
-
-	/**
-	 * Checks if the entry is a valid translation file
-	 * @param entry JarEntry to check
-	 * @return boolean if the entry is a valid translation file
-	 */
-	private static boolean isValidTranslationFile(@NotNull JarEntry entry) {
-		String entryName = entry.getName();
-		return entryName.startsWith("messages/") &&
-				!entry.isDirectory() &&
-				entryName.endsWith(".yml") &&
-				!entryName.equals("messages/template.yml");
 	}
 
 	/**
 	 * Loads the translation file into memory
 	 * @param entry JarEntry to load
 	 */
-	private static void loadTranslationFile(@NotNull JarEntry entry) {
+	private void loadTranslationFile(@NotNull JarEntry entry) {
 		String entryName = entry.getName();
 
-		try (InputStream inputStream = HatCosmetics.class.getResourceAsStream("/" + entryName)) {
+		try (InputStream inputStream = plugin.getClass().getResourceAsStream("/" + entryName)) {
 			if (inputStream != null) {
 				String locale = entry.getName().replace("messages/", "").replace(".yml", "");
 				YamlConfiguration language = YamlConfiguration.loadConfiguration(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
 				translations.put(locale, language);
 			}
 		} catch (IOException e) {
-			HatCosmetics.getLog().error("Failed to load translation file: " + entryName);
-			HatCosmetics.getLog().error(e.toString());
+            plugin.getSLF4JLogger().error("Failed to load translation file {}. {}", entryName, e.toString());
 		}
 	}
 
@@ -163,7 +153,7 @@ public class Messages {
 	 * @param path Path to the message
 	 * @return String message
 	 */
-	public static @NotNull String getMessage(String path) {
+	public @NotNull String getMessage(String path) {
 		return getMessage(serverLocale, path);
 	}
 
@@ -173,7 +163,7 @@ public class Messages {
 	 * @param path Path to the message
 	 * @return String message
 	 */
-	public static @NotNull String getMessage(@NotNull CommandSender sender, String path) {
+	public @NotNull String getMessage(@NotNull CommandSender sender, String path) {
 		if (sender instanceof Player player) {
 			return getMessage(player.locale().getLanguage(), path);
 		}
@@ -188,10 +178,10 @@ public class Messages {
 	 * @see <a href="https://www.rfc-editor.org/info/bcp47">BCP 47 - RFC</a>
 	 * @return String message
 	 */
-	private static @NotNull String getMessage(String language, String path) {
+	private @NotNull String getMessage(String language, String path) {
 		// Check if server has a forced locale or if the language is not found
 		// Use the default server locale if the language is not found
-		if (HatCosmetics.getInstance().getConfig().getBoolean("force-locale") || !translations.containsKey(language)) {
+		if (plugin.getConfig().getBoolean("force-locale") || !translations.containsKey(language)) {
 			language = serverLocale;
 		}
 
@@ -199,18 +189,18 @@ public class Messages {
 		if (yaml == null || !yaml.contains(path)) {
 			if (generics.containsKey(path)) return parseColorCodes(generics.get(path));
 
-			HatCosmetics.getLog().warn(String.format("Missing message (%s) for language %s!", path, language));
+			plugin.getSLF4JLogger().warn("Missing message ({}) for language {}!", path, language);
 			return "MISSING MESSAGE";
 		}
 
 		return parseColorCodes(yaml.getString(path));
 	}
 
-	public static void sendMessage(@NotNull CommandSender sender, @NotNull String path) {
+	public void sendMessage(@NotNull CommandSender sender, @NotNull String path) {
 		sender.sendMessage(Component.text(getMessage(sender, path)));
 	}
 
-	public static void sendMessage(@NotNull CommandSender sender, @NotNull String path, String formatArg) {
+	public void sendMessage(@NotNull CommandSender sender, @NotNull String path, String formatArg) {
 		if (formatArg == null) {
 			sendMessage(sender, path);
 		} else {
@@ -218,7 +208,7 @@ public class Messages {
 		}
 	}
 
-	public static void sendMessage(@NotNull CommandSender sender, @NotNull String path, Map<String, String> formatArgs) {
+	public void sendMessage(@NotNull CommandSender sender, @NotNull String path, Map<String, String> formatArgs) {
 		if (formatArgs == null) {
 			sendMessage(sender, path);
 		} else {
@@ -226,7 +216,7 @@ public class Messages {
 		}
 	}
 
-	public static @NotNull String format(@NotNull String format, String formatArg) {
+	public @NotNull String format(@NotNull String format, String formatArg) {
 		StringBuilder result = new StringBuilder(format.length());
 		int start = 0;
 		int openBrace = format.indexOf('{', start);
@@ -249,7 +239,7 @@ public class Messages {
 		return result.toString();
 	}
 
-	public static @NotNull String format(@NotNull String format, Map<String, String> formatArgs) {
+	public @NotNull String format(@NotNull String format, Map<String, String> formatArgs) {
 		StringBuilder result = new StringBuilder(format.length());
 		StringBuilder placeholder = new StringBuilder();
 		boolean inPlaceholder = false;
@@ -279,14 +269,25 @@ public class Messages {
 		return result.toString();
 	}
 
-	private static void logFormatWarning(String placeholder, String format) {
-		Logger logger = HatCosmetics.getLog();
+	private void logFormatWarning(String placeholder, String format) {
+		plugin.getSLF4JLogger().warn(String.format("Value for placeholder \"%s\" is not defined in the template: %s", placeholder, format));
 
-		logger.warn(String.format("Value for placeholder \"%s\" is not defined in the template: %s", placeholder, format));
-
-		if (logger.isDebugEnabled()) {
-			logger.warn("Stacktrace:", new Exception("Placeholder not defined"));
+		if (plugin.getSLF4JLogger().isDebugEnabled()) {
+			plugin.getSLF4JLogger().warn("Stacktrace:", new Exception("Placeholder not defined"));
 		}
+	}
+
+	/**
+	 * Checks if the entry is a valid translation file
+	 * @param entry JarEntry to check
+	 * @return boolean if the entry is a valid translation file
+	 */
+	private static boolean isValidTranslationFile(@NotNull JarEntry entry) {
+		String entryName = entry.getName();
+		return entryName.startsWith("messages/") &&
+				!entry.isDirectory() &&
+				entryName.endsWith(".yml") &&
+				!entryName.equals("messages/template.yml");
 	}
 
 	/**
