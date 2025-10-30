9a44839a5c800345994224835f9e4a15f6d42b5f
marcel <47798787+Supermarcel10@users.noreply.github.com>
2025-10-30 20:07:16 +0000

    - Fixup main HatCosmetics method.

--- src/main/java/me/Tonus_/hatCosmetics/HatCosmetics.java
+++ src/main/java/me/Tonus_/hatCosmetics/HatCosmetics.java
@@ -1,51 +1,32 @@
 package me.Tonus_.hatCosmetics;
 
 import lombok.Getter;
-import me.Tonus_.hatCosmetics.command.CommandHandler;
-import me.Tonus_.hatCosmetics.handler.*;
-import me.Tonus_.hatCosmetics.inventory.InventoryEventHandler;
-import me.Tonus_.hatCosmetics.inventory.InventoryHandler;
-import me.Tonus_.hatCosmetics.utility.Configs;
-import me.Tonus_.hatCosmetics.utility.VersionChecker;
-import me.Tonus_.hatCosmetics.utility.Messages;
+import me.Tonus_.hatCosmetics.config.ConfigRetriever;
 import me.Tonus_.hatCosmetics.networking.ModrinthUpstreamAPIClient;
 import me.Tonus_.hatCosmetics.updates.PluginVersionRetriever;
 import me.Tonus_.hatCosmetics.updates.SemanticVersionChecker;
+import me.Tonus_.hatCosmetics.utility.MessageHandler;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.slf4j.Logger;
-
-import java.util.*;
 
 
 public class HatCosmetics extends JavaPlugin implements Listener {
-	@Getter private static JavaPlugin instance;
-	@Getter private static Logger log; // TODO: Look to change into logHandler due to clash of getLogger()
+	private Metrics metricService;
 
-	@Getter private static CommandHandler commandHandler;
+	@Getter private ConfigRetriever configHandler;
+	@Getter private MessageHandler messageHandler;
 
 	/**
 	 * Plugin enable
 	 */
 	@Override
 	public void onEnable() {
-		instance = this;
-		log = getSLF4JLogger();
-
-		Configs.init();
-		Messages.init();
-		commandHandler = new CommandHandler();
-		InventoryHandler.init();
-		ResourcePackHandler.init();
-
-		Objects.requireNonNull(getCommand("hatcosmetics")).setExecutor(commandHandler);
-		Objects.requireNonNull(getCommand("hatcosmetics")).setTabCompleter(commandHandler);
-		getServer().getPluginManager().registerEvents(new InventoryEventHandler(), this);
-		getServer().getPluginManager().registerEvents(new PlayerJoinHandler(), this);
+		configHandler = new ConfigRetriever(this);
+		messageHandler = new MessageHandler(this, configHandler);
 
 		// Enable bStats
-		new Metrics(this, 11075);
+		metricService = new Metrics(this, 11075);
 
 		// Check for updates
 		var modrinthApiClient = new ModrinthUpstreamAPIClient(this.getSLF4JLogger(), "4h6EFh3D");
@@ -59,6 +40,7 @@ public class HatCosmetics extends JavaPlugin implements Listener {
 	 */
 	@Override
 	public void onDisable() {
+		metricService.shutdown();
 		super.onDisable();
 	}
 }
