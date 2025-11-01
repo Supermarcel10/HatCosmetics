403ee01b1f0a13eea2f6cdb1eeb3932eac74f2bb
marcel <47798787+Supermarcel10@users.noreply.github.com>
2025-11-01 12:18:15 +0000

    - Renamed HatCosmetics file to Main

--- src/main/java/me/Tonus_/hatCosmetics/Main.java
+++ src/main/java/me/Tonus_/hatCosmetics/Main.java
@@ -12,7 +12,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
-public class HatCosmetics extends JavaPlugin implements Listener {
+public class Main extends JavaPlugin implements Listener {
 	private Metrics metricService;
 
 	/**
