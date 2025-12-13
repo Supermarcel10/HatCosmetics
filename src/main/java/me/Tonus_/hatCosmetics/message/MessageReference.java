34633d41fe3f0a932180e1b058a975ae96ac2fc0
marcel <47798787+Supermarcel10@users.noreply.github.com>
2025-12-13 17:34:43 +0000

    - Updated message file structuer

--- src/main/java/me/Tonus_/hatCosmetics/message/MessageReference.java
+++ src/main/java/me/Tonus_/hatCosmetics/message/MessageReference.java
@@ -13,21 +13,22 @@ import org.jetbrains.annotations.TestOnly;
 public class MessageReference {
     public static MessageReference VERSION = cr("version");
 
-    public static MessageReference GUI_TITLE = cr("gui_title");
-    public static MessageReference GUI_CLOSE = cr("gui_close");
-    public static MessageReference GUI_NEXT = cr("gui_next");
-    public static MessageReference GUI_PREV = cr("gui_prev");
+    public static MessageReference GUI_TITLE = cr("gui.title");
+    public static MessageReference GUI_CLOSE = cr("gui.close");
+    public static MessageReference GUI_NEXT = cr("gui.next");
+    public static MessageReference GUI_PREV = cr("gui.prev");
 
-    public static MessageReference HAT_UNEQUIP = cr("hat_unequip");
-    public static MessageReference HAT_UNEQUIP_OVERLAYED = cr("hat_unequip_overlayed");
-    public static MessageReference HAT_UNEQUIP_SUCCESS = cr("hat_unequip_success");
-    public static MessageReference HAT_UNEQUIP_SUCCESS_OTHER = cr("hat_unequip_success_other");
-    public static MessageReference HAT_UNEQUIP_SUCCESS_FORCE = cr("hat_unequip_success_force");
+    public static MessageReference COSMETIC_INVENTORY_EQUIP = cr("gui.cosmetic_inv_equip");
+    public static MessageReference COSMETIC_INVENTORY_UNEQUIP = cr("gui.cosmetic_inv_unequip");
+    public static MessageReference COSMETIC_INVENTORY_UNEQUIP_OVERLAYED = cr("cosmetic_inv_unequip_overlayed");
+
+    public static MessageReference NO_HATS_GUI = cr("gui.cosmetic_list_empty");
+
+    public static MessageReference COSMETIC_UNEQUIP_SUCCESS = cr("hat_unequip_success");
+    public static MessageReference COSMETIC_UNEQUIP_SUCCESS_OTHER = cr("hat_unequip_success_other");
+    public static MessageReference COSMETIC_UNEQUIP_SUCCESS_FORCE = cr("hat_unequip_success_force");
 
     public static MessageReference NO_PERMISSION = cr("no_permission");
-    public static MessageReference NOT_ONLINE = cr("not_online");
-
-    public static MessageReference HAT_EQUIP = cr("hat_equip");
 
     public static MessageReference HAT_NOT_EXIST = cr("hat_not_exist");
     public static MessageReference HAT_SUCCESS = cr("hat_success");
@@ -38,7 +39,8 @@ public class MessageReference {
     public static MessageReference NO_HAT_OTHER = cr("no_hat_other");
     public static MessageReference NO_HAT_PERMISSION = cr("no_hat_permission");
     public static MessageReference NO_HAT_PERMISSION_OTHER = cr("no_hat_permission_other");
-    public static MessageReference NO_HATS_GUI = cr("no_hats_gui");
+
+    public static MessageReference PLAYER_NOT_ONLINE = cr("player_not_online");
 
     public static MessageReference RELOAD_SUCCESS = cr("reload_success");
 
