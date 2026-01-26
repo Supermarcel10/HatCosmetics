368ed6b6eaa27dd7d655878b5a9b07ae5c8ca4ce
Jukoz <olegptk12@gmail.com>
2026-01-26 12:44:41 -0500

    Remove unused imports

--- src/main/java/me/Tonus_/hatCosmetics/events/MainHatsCommand.java
+++ src/main/java/me/Tonus_/hatCosmetics/events/MainHatsCommand.java
@@ -9,13 +9,11 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
-import java.util.Objects;
 
 public class MainHatsCommand implements CommandExecutor {
     private final Main main;
