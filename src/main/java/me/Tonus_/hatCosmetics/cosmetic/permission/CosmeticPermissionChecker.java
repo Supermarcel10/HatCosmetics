0d2d31f1cfab0e22003d7401884dc94a89ba1f2b
marcel <47798787+Supermarcel10@users.noreply.github.com>
2026-05-25 18:07:59 +0100

    - Further cleanup

--- src/main/java/me/Tonus_/hatCosmetics/cosmetic/permission/CosmeticPermissionChecker.java
+++ src/main/java/me/Tonus_/hatCosmetics/cosmetic/permission/CosmeticPermissionChecker.java
@@ -1,12 +1,10 @@
 package me.Tonus_.hatCosmetics.cosmetic.permission;
 
-import lombok.RequiredArgsConstructor;
 import me.Tonus_.hatCosmetics.cosmetic.Cosmetic;
 import me.Tonus_.hatCosmetics.permissions.PermissionNode;
 import org.bukkit.entity.Player;
 
 
-@RequiredArgsConstructor
 public class CosmeticPermissionChecker implements ICosmeticPermissionChecker {
     @Override
     public boolean canUseCosmetic(Player player, Cosmetic cosmetic) {
